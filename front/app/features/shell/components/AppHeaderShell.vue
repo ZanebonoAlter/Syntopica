844c54c9f9b625077aee92960cc72edbceb41be5
root <root@DESKTOP-2GJ77UG>
2026-05-29 13:43:23 +0800

    feat(daily-report): add migrations for daily_report_threads table

--- front/app/features/shell/components/AppHeaderShell.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import AppHeaderView from '~/features/shell/components/AppHeaderView.vue'
-</script>
-
-<template>
-  <AppHeaderView v-bind="$attrs" />
-</template>
--- front/app/pages/tags.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import TagsPage from '~/features/tags/components/TagsPage.vue'
-</script>
-
-<template>
-  <TagsPage />
-</template>
